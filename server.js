3938b3b97005eddc5688cb929e064fdd7ff79c91
pandeyg00121 <pranaypandey00121@gmail.com>
2023-10-18 04:46:21 +0530

    Server file seperated

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const app= require('./app');
 
-const port=3000;
+const dotenv = require('dotenv');
+//requiring dotenv module
+dotenv.config({ path: './config.env' });
+//specifying the path of env variables file
+
+const app = require('./app');
+const port=process.env.PORT || 3000;
+// console.log(process.env);
 
 app.listen(port,()=>{
 
