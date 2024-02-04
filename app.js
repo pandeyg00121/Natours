4b04b56a711023389f1cd453bc790d47869329f0
pandeyg00121 <pranaypandey00121@gmail.com>
2024-02-04 23:28:04 +0530

    XSS and NOSQL attacks proof

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const fs = require("fs");
-const morgan = require("morgan");
 const express = require("express");
+const morgan = require("morgan");
 const ratelimit = require("express-rate-limit");
-
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 var bodyParser = require("body-parser");
 
 const AppError = require("./utils/appError");
@@ -12,28 +14,53 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// GLOBAL MIDDLEWARES
-app.use(morgan("dev"));
+// GLOBAL MIDDLEWARES------>
 
+//Set security HTTP headers
+app.use(helmet());
+//for develpment and status code on console
+app.use(morgan("dev"));
+//for rate limiting of requests
 const limiter = ratelimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many request at same time from your IP address..Hold your Horses!!",
 });
 app.use("/api", limiter);
-app.use(bodyParser.urlencoded({ extended: false }));
+
+//reading data from req.body
+app.use(bodyParser.urlencoded({ limit: '10kb', extended: false }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+//prevent parameter pollution
+app.use(
+    hpp({
+      whitelist: [
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+      ]
+    })
+  );
+//middleware to serve static files
 app.use(express.static(`${__dirname}/public`));
-//middleware to view static files
+
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   //ISOString converts timestamp to readable string
-  console.log(req.headers);
   next();
 });
 
 //(3) ROUTES
-
 app.use("/api/v1/tours", tourRouter); //middleware
 //this is called mounting a router on a route.
 app.use("/api/v1/users", userRouter);
