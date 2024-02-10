f8cf1028d02c8769ee7e249a66c2187e9cd1081a
pandeyg00121 <pranaypandey00121@gmail.com>
2024-02-11 03:01:12 +0530

    C:/Program Files/Git/me endpoint defined

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,6 +17,12 @@ const filterObj = (obj, ...allowedFields) => {
   return newobj;
 };
 
+//to see your details by factory handler getOne
+exports.getMe = (req,res,next) =>{
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   //1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
